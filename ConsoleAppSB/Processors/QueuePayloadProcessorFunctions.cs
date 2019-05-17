a8aaa780388c2fa65bce7f26771e0fa6f3202062
Hemant Shelar <hemant.shelar@gmail.com>
2019-05-17 19:59:27 +1000

    Implemented DI

---
+++ ConsoleAppSB/Processors/QueuePayloadProcessorFunctions.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleAppSB.Interfaces;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppSB.Processors
+{
+	public class QueuePayloadProcessorFunctions
+	{
+		private readonly IDataStore dataStore;
+		public QueuePayloadProcessorFunctions(IDataStore dataStore)
+		{
+			this.dataStore = dataStore;
+		}
+		public void ProcessQueueMessage([QueueTrigger("myqueue")] string message, ILogger logger)
+		{
+			logger.LogInformation(message);
+			this.dataStore.ProcessData();
+		}
+	}
+}
--- ConsoleAppSB/Program.cs
+++ ConsoleAppSB/Program.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ConsoleAppSB.Interfaces;
+using ConsoleAppSB.Services;
+using Microsoft.Azure.WebJobs.Host;
 
 namespace ConsoleAppSB
 {
@@ -16,16 +20,16 @@ namespace ConsoleAppSB
 			//Below piece of code is not required if name of json file is 'appsettings.json'
 			builder.ConfigureAppConfiguration(c =>
 		   {
-			   c.AddJsonFile("myAppsettings.json", optional: false, reloadOnChange: true);
-
-		   });
-			builder.ConfigureWebJobs(b =>
+			   c.AddJsonFile("myAppsettings.json", optional: true, reloadOnChange: true);
+			   c.AddEnvironmentVariables();
+		   })
+			.ConfigureWebJobs(b =>
 			{
 				b.AddAzureStorageCoreServices();
 				b.AddAzureStorage();
-			});
-
-			builder.ConfigureLogging((context, b) =>
+				b.Services.AddSingleton<IDataStore, DataStore>();
+			})
+			.ConfigureLogging((context, b) =>
 			{
 				b.AddConsole();
 			});
@@ -38,11 +42,17 @@ namespace ConsoleAppSB
 	}
 
 
-	public static class Functions
-	{
-		public static void ProcessQueueMessage([QueueTrigger("myqueue")] string message, ILogger logger)
-		{
-			logger.LogInformation(message);
-		}
-	}
+	//public class Functions
+	//{
+	//	private readonly IDataStore dataStore;
+	//	public Functions(IDataStore dataStore)
+	//	{
+	//		this.dataStore = dataStore;
+	//	}
+	//	public void ProcessQueueMessage([QueueTrigger("myqueue")] string message, ILogger logger)
+	//	{
+	//		logger.LogInformation(message);
+	//		this.dataStore.ProcessData();
+	//	}
+	//}
 }
